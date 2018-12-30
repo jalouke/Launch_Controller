36d850bf868d15d566d9b65c3c6db3c36f3ce3e1
Jeremy <jalouke@gmail.com>
2018-12-30 13:06:39 -0800

    working progress on circuit and layout. using MOSFETs for ignition circuit

--- Keypad-rs485-recieve-test/Keypad-rs485-recieve-test.ino
+++ Keypad-rs485-recieve-test/Keypad-rs485-recieve-test.ino
@@ -11,69 +11,69 @@ boolean LEDpinC_state;
 boolean LEDpinD_state;
 int key = 0;
 int temp;
+int sensor = 0;
 
 void setup() {
   Serial.begin(9600);
   while (!Serial);
-
   RS485.begin(9600);
-
   // enable reception, can be disabled with: RS485.noReceive();
   RS485.receive();
 
-  DDRB |= 0B00111100;  //port PB5,4,3,2 output
-//  pinMode(LEDpinA, OUTPUT);
-//  pinMode(LEDpinB, OUTPUT);
-//  pinMode(LEDpinC, OUTPUT);
-//  pinMode(LEDpinD, OUTPUT);
+  DDRB |= 0B00111110;  //port PB5,4,3,2 output
 }
 
 void loop() {
-  if (RS485.available()) {
+  sensor = analogRead(A0);
+  Serial.println(sensor);
+  while (RS485.available()) {
+    sensor = analogRead(A0);
+    Serial.println(sensor);
     key = RS485.read();
     Serial.write(key);
-  }
-  if (key == 'A'){
-    PORTB = PORTB ^B00100000;  // Remember LED state, lit or unlit.
-  }
-  else if (key == 'B'){
-    PORTB = PORTB ^B00010000;  // Remember LED state, lit or unlit.
-  }
-  else if (key == 'C'){
-    PORTB = PORTB ^B00001000;  // Remember LED state, lit or unlit.
-  }
-  else if (key == 'D'){
-    PORTB = PORTB ^B00000100;  // Remember LED state, lit or unlit.
-  }
-  else if (key == '#'){
-    int count = 1;
-    temp = PORTB;
-    while (count<=5){
-      PORTB = 0B00111100;
-      delay(175);
-      PORTB = PORTB ^ 0B00111100;
-      delay(75);
-      count++;
+    if (key == 'A'){
+      PORTB = PORTB ^B00100000;  // Remember LED state, lit or unlit.
     }
-    PORTB = temp;
-  }        
-  else if (key == '*'){
-    int count = 1;
-    temp = PORTB;
-    while (count<=5){
-      PORTB = 0B00100000;
-      delay(50);
-      PORTB = PORTB >>1;
-      delay(50);
-      PORTB = PORTB >>1;
-      delay(50);
-      PORTB = PORTB >>1;
-      delay(50);
-      PORTB = 0x00;
-      delay(50);
-      count++;
+    else if (key == 'B'){
+      PORTB = PORTB ^B00010000;  // Remember LED state, lit or unlit.
     }
-    PORTB = temp;
+    else if (key == 'C'){
+      PORTB = PORTB ^B00001000;  // Remember LED state, lit or unlit.
+    }
+    else if (key == 'D'){
+      PORTB = PORTB ^B00000100;  // Remember LED state, lit or unlit.
+    }
+    else if (key == '#'){
+      int count = 1;
+      temp = PORTB;
+      while (count<=5){
+        PORTB = 0B00111100;
+        delay(175);
+        PORTB = PORTB ^0B00111100;
+        delay(75);
+        count++;
+      }
+      PORTB = temp;
+    }        
+    else if (key == '*'){
+      int count = 1;
+      temp = PORTB;
+      while (count<=5){
+        PORTB = 0B00100000;
+        delay(50);
+        PORTB = PORTB ^0B00110000;
+        delay(50);
+        PORTB = PORTB ^0B00011000;
+        delay(50);
+        PORTB = PORTB ^0B00001100;
+        delay(50);
+        PORTB = PORTB ^0B00000100;
+        delay(50);
+        count++;
+      }
+      tone(9, 6000, 500); // 20-24 microseconds
+      delay(1000);
+      PORTB = temp;
+    }
   }
-  Serial.write(key);
 }
--- Keypad-rs485-transmit-test/Keypad-rs485-transmit-test.ino
+++ Keypad-rs485-transmit-test/Keypad-rs485-transmit-test.ino
@@ -9,6 +9,8 @@
 */
 #include <Keypad.h>
 #include <ArduinoRS485.h>
+#include <Wire.h>
+#define DISPLAY_ADDRESS1 0x72 //This is the default address of the LCD display
 
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //four columns
@@ -25,18 +27,55 @@ byte colPins[COLS] = {A3,A2,A1,A0}; //connect to the column pinouts of the keypa
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 void setup(){
-  Serial.begin(9600);
+  Wire.begin(); // LCD begin
   RS485.begin(9600);
   Serial.println("start");
+
+  //LCD initialization and color selection
+  Wire.begin(DISPLAY_ADDRESS1);
+  Wire.write('|');
+  Wire.write('-');
+  delay(20);
+  Wire.write('|'); //Put LCD into setting mode
+  Wire.write(128+29); //red
+  delay(20);
+  Wire.write('|');
+  Wire.write(158+29); //green
+  delay(20);
+  Wire.write('|');
+  Wire.write(188+29); //blue
+  delay(20);
+  Wire.end();
+  delay(5000);
+  //write initialized text
+  Wire.begin(DISPLAY_ADDRESS1);
+  Wire.write('|');
+  Wire.write('-');
+  Wire.print("Range:"); //number starts at pos 7
+  delay(50);
+  Wire.write(254); //Send cursor command 
+  Wire.write(128 + 64 + 0); //cursor position (128), Line 2 (64), begin of line (0)
+  Wire.print("Launch Pad:"); //number starts at pos 12
+  delay(50);
+  Wire.write(254); //Send cursor command 
+  Wire.write(128 + 84 + 0); //cursor position (128), Line 4 (84), begin of line (0)
+  Wire.print("ARM?:"); //number starts at pos 5
+  delay(50);
+  Wire.end();
 }
   
 void loop(){
   char key = keypad.getKey();
   
+  
+  
   if (key){
     RS485.beginTransmission();
-    //Serial.println(key);
     RS485.println(key);
     RS485.endTransmission();
+    Wire.write(254); //Send cursor command 
+    Wire.write(128 + 64 + 12); //cursor position (128), Line 4 (84), begin of line (0)
+    Wire.print(key);
+    
   }
 }
